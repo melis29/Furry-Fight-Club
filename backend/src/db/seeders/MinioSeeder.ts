876cd25ae7e0173b42073c54c91c638f7356d158
melis29 <melissa.nguyen187@gmail.com>
2023-06-14 22:48:04 -0400

    Added mino default

--- backend/src/db/seeders/MinioSeeder.ts
+++ backend/src/db/seeders/MinioSeeder.ts
@@ -7,11 +7,29 @@ export class MinioSeeder extends Seeder {
     async run(em: EntityManager, context: Dictionary): Promise<void> {
 
         // Cheeky throw dog.jpg into Minio
-        const dogfile = await fs.readFile("./test/dog.jpg");
+        const dogfile = await fs.readFile("1.jpg");
         const file = {
             file: dogfile,
-            filename: "dog.jpg",
+            filename: "1.jpg",
         }
         await UploadFileToMinio(file);
+        const dog2file = await fs.readFile("2.jpg");
+        const file2 = {
+            file2: dog2file,
+            filename: "2.jpg",
+        }
+        await UploadFileToMinio(file2);
+        const dogfile3 = await fs.readFile("3.jpg");
+        const file3 = {
+            file3: dogfile3,
+            filename: "3.jpg",
+        }
+        await UploadFileToMinio(file3);
+        const dogfile4 = await fs.readFile("1.jpg");
+        const file4 = {
+            file4: dogfile4,
+            filename: "4.jpg",
+        }
+        await UploadFileToMinio(file4);
     }
 }
